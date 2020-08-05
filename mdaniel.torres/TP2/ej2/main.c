9aeb18101557497f16a705b72ef876d6373ea445
Marcos Daniel Torres <marcoss.nqn@gmail.com>
2020-08-05 20:46:29 -0300

    Arreglé espacios

--- mdaniel.torres/TP2/ej2/main.c
+++ mdaniel.torres/TP2/ej2/main.c
@@ -16,90 +16,90 @@ void contador();
 
 int main(void)
 {	
-	led_init();
-	pulsador_init();
-	while (1) {
-		sePresiono();
-		if(!boton){
-			contador();
-		}else{
-			esperar();
-			prender_led();
+    led_init();
+    pulsador_init();
+        while (1) {
+            sePresiono();
+        if(!boton){
+            contador();
+        }else{
+            esperar();
+            prender_led();
 
-			esperar();
-			apagar_led();
-		}
-    	}
+            esperar();
+                apagar_led();
+        }
+    }
 }
 
 void contador(){
-unsigned char* PUERTO_B = 0x25;
+    unsigned char* PUERTO_B = 0x25;
 
-esperar();
-*(PUERTO_B) = *(PUERTO_B) ^ (0x02);
-esperar();
-*(PUERTO_B) = *(PUERTO_B) ^ (0x02);
-*(PUERTO_B) = *(PUERTO_B) ^ (0x04);
-esperar();
-*(PUERTO_B) = *(PUERTO_B) ^ (0x02);
-esperar();
-*(PUERTO_B) = *(PUERTO_B) ^ (0x02);
-*(PUERTO_B) = *(PUERTO_B) ^ (0x04);
-*(PUERTO_B) = *(PUERTO_B) ^ (0x08);
-esperar();
-*(PUERTO_B) = *(PUERTO_B) ^ (0x02);
-esperar();
-*(PUERTO_B) = *(PUERTO_B) ^ (0x02);
-*(PUERTO_B) = *(PUERTO_B) ^ (0x04);
-esperar();
-*(PUERTO_B) = *(PUERTO_B) ^ (0x02);
-esperar();
-led_init();
+    esperar();
+    *(PUERTO_B) = *(PUERTO_B) ^ (0x02);
+    esperar();
+    *(PUERTO_B) = *(PUERTO_B) ^ (0x02);
+    *(PUERTO_B) = *(PUERTO_B) ^ (0x04);
+    esperar();
+    *(PUERTO_B) = *(PUERTO_B) ^ (0x02);
+    esperar();
+    *(PUERTO_B) = *(PUERTO_B) ^ (0x02);
+    *(PUERTO_B) = *(PUERTO_B) ^ (0x04);
+    *(PUERTO_B) = *(PUERTO_B) ^ (0x08);
+    esperar();
+    *(PUERTO_B) = *(PUERTO_B) ^ (0x02);
+    esperar();
+    *(PUERTO_B) = *(PUERTO_B) ^ (0x02);
+    *(PUERTO_B) = *(PUERTO_B) ^ (0x04);
+    esperar();
+    *(PUERTO_B) = *(PUERTO_B) ^ (0x02);
+    esperar();
+    led_init();
 }
 
 void led_init(){
-volatile unsigned char* DDR_B = 0x24;
-volatile unsigned char* PUERTO_B = 0x25;
+    volatile unsigned char* DDR_B = 0x24;
+    volatile unsigned char* PUERTO_B = 0x25;
 
 
-//primero
-*(PUERTO_B) = *(PUERTO_B) & (~ 0x02);
+    //primero
+    *(PUERTO_B) = *(PUERTO_B) & (~ 0x02);
 
-*(DDR_B) = *(DDR_B) | (0x02);
+    *(DDR_B) = *(DDR_B) | (0x02);
 
-//segundo
+    //segundo
 
-*(PUERTO_B) = *(PUERTO_B) & (~ 0x04);
+    *(PUERTO_B) = *(PUERTO_B) & (~ 0x04);
 
-*(DDR_B) = *(DDR_B) | (0x04);
+    *(DDR_B) = *(DDR_B) | (0x04);
 
-//tercero
-*(PUERTO_B) = *(PUERTO_B) & (~ 0x08);
+    //tercero
+    *(PUERTO_B) = *(PUERTO_B) & (~ 0x08);
 
-*(DDR_B) = *(DDR_B) | (0x08);
+    *(DDR_B) = *(DDR_B) | (0x08);
 
 }
 
 void pulsador_init(){
-volatile unsigned char* DDR_B = 0x24;
-volatile unsigned char* PUERTO_B = 0x25;
+    volatile unsigned char* DDR_B = 0x24;
+    volatile unsigned char* PUERTO_B = 0x25;
 
-//pulsador
-*(PUERTO_B) = *(PUERTO_B) | (0x10);
-//*(PUERTO_B) = *(PUERTO_B) & (~0x10);
+    //pulsador
+    *(PUERTO_B) = *(PUERTO_B) | (0x10);
+    //*(PUERTO_B) = *(PUERTO_B) & (~0x10);
 
-*(DDR_B) = *(DDR_B) & (~0x10);
+    *(DDR_B) = *(DDR_B) & (~0x10);
 
 }
 
 int sePresiono(){
-unsigned char* PIN_B = 0x23;
+    unsigned char* PIN_B = 0x23;
 
-// verificar si el bit 0x10 de puertoB esta setteado
-if((*PIN_B) & (0x10)){
-	boton = 1;
-	return 1;
-}
-boton = 0;
-return 1;
+    // verificar si el bit 0x10 de puertoB esta setteado
+    if((*PIN_B) & (0x10)){
+        boton = 1;
+        return 1;
+    }
+    boton = 0;
+    return 1;
 }
