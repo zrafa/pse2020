c258d33b21246ef98f1c40b3f3e1de4191cef23e
Cristian Vincenzini <borgesvive@hotmail.com>
2020-08-17 19:21:08 -0400

    Agrega TP3

---
+++ cristian.vincenzini/TP3/main.c
@@ -0,0 +1,56 @@
+/**********************************************************************
+ *
+ * main.c - the main program test file for the serial driver
+ *
+ **********************************************************************/
+
+#include "serial.h"
+#include "utils.h"
+
+int main(void)
+{
+    char rcvChar = 0;
+    int i;
+
+    /* Configure the UART for the serial driver */
+    serial_init();
+
+    /* Configure pins */
+    leds_init();
+
+    serial_put_str("Start");
+
+    while (rcvChar != 'q')
+    {
+        /* Wait for an incoming character */
+        rcvChar = serial_get_char();
+
+        switch (rcvChar) {
+            case 'c':
+                serial_put_str("Comenzando suma binaria");
+                suma_binaria();
+                serial_put_str("Fin suma binaria");
+                break;
+            case 'k':
+                serial_put_str("Comenzando knight-rider");
+                for (i = 0; i < 10; i++) {
+                    knight_rider();
+                }
+                serial_put_str("Fin knight-rider");
+                break;
+            default:
+                /* Echo the character back along with a carriage return and line feed */
+                serial_put_char(rcvChar);
+                serial_put_char('\r');
+                serial_put_char('\n');
+                break;
+        }
+    }
+
+    serial_put_str("Fin aplicacion");
+
+    for (;;);
+
+    return 0;
+}
+
---
+++ cristian.vincenzini/TP3/utils.h
@@ -0,0 +1,13 @@
+
+/* utils.h - definicion de funciones utiles al TP2 */
+
+#ifndef _UTILS_H
+#define _UTILS_H
+
+void leds_init();
+void esperar(unsigned long);
+void knight_rider();
+void suma_binaria();
+unsigned char leer_boton();
+
+#endif	/* _UTILS_H */
