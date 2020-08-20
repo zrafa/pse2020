91398b67231f1b985797976336a2212cc2d6f417
katherina navarrete <katherina.navarrete@est.fi.uncoma.edu.ar>
2020-08-20 15:22:32 -0300

    Agrego resolucion de tp3

---
+++ katherina.navarrete/TP3/utils.c
@@ -0,0 +1,107 @@
+
+/* utils.c - funciones utiles al TP2 */
+
+/* Enciende y apaga un led conectado al puerto B bit 5 de un atmega328
+ * El puerto B de un atmega328 tiene los bits 0-5 mapeados a los 
+ * pines 8-13 de arduino 
+ */
+
+#define prende (0x3F)
+
+
+/* puertos de E/S */
+volatile unsigned char * puerto_b = (unsigned char *) 0x25; /* direccion de PORTB: SALIDA */
+volatile unsigned char * ddr_b = (unsigned char *) 0x24; /* direccion de DDR B (registro de control) */
+volatile unsigned char * pin_b = (unsigned char *) 0x23; /* direccion PIN B (registro de datos)*/
+
+/* 
+ * Configuración: Establecer el 5to bit del puerto B como entrada
+ *
+ * 	Poner en '0' el 5to bit de la dirección 0x24, que 
+ * 	es el la dirección del registro de control
+ *	(direccion de los datos) del puerto B (DDRB - Data Direction Register).
+ *      El 5to bit define la entrada o salida del
+ * 	pin del atmega328p que tiene conectado un led en una board arduino
+ */
+void let_init(){
+	*(puerto_b) = *(puerto_b) & (~ prende);
+	*(ddr_b) = *(ddr_b) | (prende);
+}
+
+void esperar() {
+	volatile unsigned long i;
+	for (i=0; i<950000; i++);
+}
+void esperarMenos(){
+	volatile unsigned long i;
+	for (i=0; i<23750; i++);
+}
+
+
+void prender_led() {
+	volatile unsigned char valor_b = *(puerto_b);
+	valor_b |=  prende ;
+	*(puerto_b) = valor_b;
+}
+
+void apagar_led() {
+	volatile unsigned char valor_b = *(puerto_b);
+	valor_b &= ~(prende);
+	*(puerto_b) = valor_b;
+}
+
+void contar(unsigned char num){
+	volatile unsigned char valor_b = *(puerto_b);
+	valor_b ^= num;
+	*(puerto_b) = valor_b;
+	
+}
+void contarBin(){
+	apagar_led(); 
+	esperar();
+	contar(0x08);
+	esperar();
+	contar(0x18);
+	esperar();
+	contar(0x08);
+	esperar();
+	contar(0x38);
+	esperar();
+	contar(0x08);
+	esperar();
+	contar(0x18);
+	esperar();
+	contar(0x08);
+	esperar();
+	apagar_led(); 
+}
+void knight_rider(){
+	volatile unsigned char valor_b = *(puerto_b);
+	volatile unsigned num = 0x20;
+        
+	valor_b ^= num;
+	num = num ^ (num >> 1);
+	*(puerto_b) = valor_b;
+	esperarMenos();
+	while(num != 1){
+		
+		valor_b ^= num;
+		num = (num >> 1);
+		*(puerto_b) = valor_b;
+		esperarMenos();
+	}
+	num = num ^ (num << 1);
+	*(puerto_b) = valor_b;
+	esperarMenos();
+	while(num != 48){
+		
+		valor_b ^= num;
+		num = (num << 1);
+		*(puerto_b) = valor_b;
+		esperarMenos();
+	}
+apagar_led();
+}
+
+
+
