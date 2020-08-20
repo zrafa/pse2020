517995cb3e742fb211dff43837f6d2080d7c09d7
Karen Flores <karen.flores@est.fi.uncoma.edu.ar>
2020-08-20 13:48:39 -0300

    Agrego solucion TP3

---
+++ karen.flores/TP3/Ej_4/main.c
@@ -0,0 +1,56 @@
+/**********************************************************************
+ *
+ * main.c - the main program test file for the serial driver
+ *
+ **********************************************************************/
+
+#include "serial.h"
+#include "utils.h"
+
+int main(void)
+{
+    char rcvChar = 0;
+
+    /* Configure the UART for the serial driver */
+    serial_init();
+
+    serial_put_char('s');
+    serial_put_char('t');
+    serial_put_char('a');
+    serial_put_char('r');
+    serial_put_char('t');
+    serial_put_char('\r');
+    serial_put_char('\n');
+
+    while (rcvChar != 'q')
+    {
+        /* Wait for an incoming character */
+        rcvChar = serial_get_char();
+	serial_put_char(rcvChar);
+	if(rcvChar == 'c') {
+		let_init(); 
+		binario();
+		apagar_led();
+	}
+	if(rcvChar == 'k') {
+		led_init(); 
+		knight_rider();
+		knight_rider_reverso();
+		knight_rider();
+		knight_rider_reverso();
+		knight_rider();
+		knight_rider_reverso();
+		knight_rider();
+		knight_rider_reverso();
+		apagar_led();
+	}	
+        /* Echo the character back along with a carriage return and line feed */
+        
+    }
+    serial_put_char('\r');
+    serial_put_char('\n');
+
+    for (;;);
+
+    return 0;
+}
---
+++ karen.flores/TP3/Ej_4/utils_conteo.c
@@ -0,0 +1,92 @@
+
+/* utils.c - funciones utiles al TP2 */
+
+/* Enciende y apaga un led conectado al puerto B bit 5 de un atmega328
+ * El puerto B de un atmega328 tiene los bits 0-5 mapeados a los 
+ * pines 8-13 de arduino 
+ */
+#define LED (0x38)
+#define BIT2 (0x04)
+
+/* puertos de E/S */
+volatile unsigned char * puerto_b = (unsigned char *) 0x25; /* direccion de PORTB: SALIDA */
+volatile unsigned char * ddr_b = (unsigned char *) 0x24; /* direccion de DDR B (registro de control) */
+volatile unsigned char * pin_b = (unsigned char *) 0x23; /* direccion PIN B (registro de datos)*/
+volatile unsigned char estado;
+
+
+/* 
+ * Configuración: Establecer el 5to bit del puerto B como entrada
+ *
+ * 	Poner en '0' el 5to bit de la dirección 0x24, que 
+ * 	es el la dirección del registro de control
+ *	(direccion de los datos) del puerto B (DDRB - Data Direction Register).
+ *      El 5to bit define la entrada o salida del
+ * 	pin del atmega328p que tiene conectado un led en una board arduino
+ */
+void let_init(){
+	*(puerto_b) = *(puerto_b) & (~ 0x3C);
+	*(ddr_b) = *(ddr_b) | (LED);
+	*(ddr_b) = *(ddr_b) & (~ BIT2);
+	*(pin_b) =  0x04;
+}
+
+void esperar() {
+	volatile unsigned long i;
+	/* delay de 1/2 segundo */
+	for (i=0; i<225000; i++);
+}
+
+void prender_led() {
+	volatile unsigned char valor_b = *(puerto_b);
+	valor_b |=  LED ;
+	*(puerto_b) = valor_b;
+}
+
+void apagar_led() {
+	volatile unsigned char valor_b = *(puerto_b);
+	valor_b &= ~(LED);
+	*(puerto_b) = valor_b;
+}
+
+unsigned char get_estado(){
+	volatile unsigned char valor_b = *(pin_b);
+	valor_b &=  BIT2 ;
+	if(valor_b != BIT2){
+		if(estado == 0){estado =1;}
+		else{estado = 0;}
+	}
+	
+	return estado;
+}
+
+void binario(){
+	volatile unsigned char valor_b = *(pin_b);
+	valor_b ^= 0x20;
+	*(puerto_b) = valor_b;
+	esperar();
+	
+	valor_b ^= 0x30;
+	*(puerto_b) = valor_b;
+	esperar();
+	
+	valor_b ^= 0x20;
+	*(puerto_b) = valor_b;
+	esperar();
+	
+	valor_b ^= 0x38;
+	*(puerto_b) = valor_b;
+	esperar();
+
+	valor_b ^= 0x20;
+	*(puerto_b) = valor_b;
+	esperar();
+
+	valor_b ^= 0x30;
+	*(puerto_b) = valor_b;
+	esperar();
+	
+	valor_b ^= 0x20;
+	*(puerto_b) = valor_b;
+	esperar();
+}
